9a8bf9832a995c2246c06e247e5ba3bd1b52e227
Bobby McCann <bobby.mccann@spareroom.co.uk>
2022-04-01 11:34:02 +0100

    Post body

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -28,4 +30,27 @@ func GetLocalisation(w http.ResponseWriter, r *http.Request) {
 		Original:   "this is the original text",
 		Translated: "this is the translated text",
 	})
+}
+
+func PostLocalisation(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 10000))
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	var l Localisation
+	if err := json.Unmarshal(body, &l); err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+	// TODO: write our localisation
+
+	// Send it back to the client
+	json.NewEncoder(w).Encode(l)
 }
